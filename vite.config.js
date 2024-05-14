93f5209a3168740eb797de0ab1171b871b8ff42e
Simeon <vecksimeon@gmail.com>
2024-05-14 15:23:33 +0100

    trying to build warnings

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-import getEnvModule from './env'
-import dns from 'dns'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import getEnvModule from "./env";
+import dns from "dns";
 
-dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder("verbatim");
 
 getEnvModule().setEnvironmentVarsFromTestEnv(__dirname);
 
@@ -13,37 +13,40 @@ process.env.CLIENT_ID = process.env.SPA_CLIENT_ID || process.env.CLIENT_ID;
 const env = {};
 
 // List of environment variables made available to the app
-[
-  'ISSUER',
-  'CLIENT_ID'
-].forEach((key) => {
-  if (!process.env[key]) {
-    throw new Error(`Environment variable ${key} must be set. See README.md`);
-  }
-  env[key] = process.env[key];
+["ISSUER", "CLIENT_ID"].forEach((key) => {
+    if (!process.env[key]) {
+        throw new Error(`Environment variable ${key} must be set. See README.md`);
+    }
+    env[key] = process.env[key];
 });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': env
-  },
-  resolve: {
-    alias: {
-      'react-router-dom': path.resolve(__dirname, 'node_modules/react-router-dom')
-    }
-  },
-  server: {
-    port: process.env.PORT || 3000
-  },
-  build: {
-    rollupOptions: {
-      // always throw with build warnings
-      onwarn (warning, warn) {
-        warn('\nBuild warning happened, customize "onwarn" callback in vite.config.js to handle this error.');
-        //throw new Error(warning);
-      }
-    }
-  }
-})
+    plugins: [react()],
+    define: {
+        "process.env": env,
+    },
+    resolve: {
+        alias: {
+            "react-router-dom": path.resolve(__dirname, "node_modules/react-router-dom"),
+        },
+    },
+    server: {
+        port: process.env.PORT || 3000,
+    },
+    build: {
+        rollupOptions: {
+            // always throw with build warnings
+            onwarn(warning, warn) {
+                if (warning.code !== "CIRCULAR_DEPENDENCY") {
+                    warn(
+                        '\nBuild warning happened, customize "onwarn" callback in vite.config.js to handle this error.'
+                    );
+                    console.log(warning);
+                    //throw new Error(warning);
+                }
+            },
+        },
+        outDir: "build",
+    },
+});
